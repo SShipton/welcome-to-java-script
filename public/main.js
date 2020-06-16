6d493acf4e62d8e28504687df344be708f9d1bcc
Skylar Shipton <skylarshipton@outlook.com>
2020-06-15 22:38:02 -0400

    The progress I made felt like a micro CodeWar. It won't let me save it without the comma after the 40 in my array, for some reason.

--- public/main.js
+++ public/main.js
@@ -1,7 +1,37 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const fullName = 'Skylar'
+const numberOfCupsOfCoffee = 0
+
+console.log(
+  `Hi! My name is ${fullName}, and I drink ${numberOfCupsOfCoffee} cups of coffee every day.`
+)
+
+const numbers = [
+  41,
+  89,
+  93,
+  61,
+  51,
+  84,
+  36,
+  3,
+  39,
+  54,
+  83,
+  49,
+  24,
+  30,
+  50,
+  22,
+  22,
+  53,
+  65,
+  40,
+]
+let smallest = numbers[0]
+
+for (let index = 1; index < numbers.length; index++) {
+  if (numbers[index] < smallest) {
+    smallest = numbers[index]
   }
 }
-
-document.addEventListener('DOMContentLoaded', main)
+console.log(smallest)
